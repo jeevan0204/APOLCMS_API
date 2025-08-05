1b56cd499723e67b7fcb016ff1ec40b9efc3677e
Jeevan Kumar Payam <1091@DOMAIN.APCFSS.IN>
2025-08-05 11:32:41 +0530

    updated file

--- src/main/java/in/apcfss/requestbodies/LoginRequest.java
+++ src/main/java/in/apcfss/requestbodies/LoginRequest.java
@@ -17,6 +17,30 @@ public class LoginRequest {
 	private String password;
 	private String captcha;
 	private String captchaId;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getCaptcha() {
+		return captcha;
+	}
+	public void setCaptcha(String captcha) {
+		this.captcha = captcha;
+	}
+	public String getCaptchaId() {
+		return captchaId;
+	}
+	public void setCaptchaId(String captchaId) {
+		this.captchaId = captchaId;
+	}
 	
 
 }
